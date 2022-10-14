5a412ec3aa0c82f4e72a5006e2fcc2c8b63b2601
Andrey Zbukar <andrey.zbk@gmail.com>
2022-10-14 18:10:30 +0300

    Blog: Previous / Next story [YUOPENY-15]

---
+++ openy_node/modules/openy_node_blog/src/BlogSimplePaginator.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Drupal\openy_node_blog;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Routing\RouteMatchInterface;
+
+class BlogSimplePaginator {
+
+  /**
+   * The node storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $nodeStorage;
+
+  /**
+   * The route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Constructs the BlogSimplePaginator.
+   *
+   * @param EntityTypeManagerInterface $entity_type_manager
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, RouteMatchInterface $current_route_match) {
+    $this->nodeStorage = $entity_type_manager->getStorage('node');
+    $this->routeMatch = $current_route_match;
+  }
+
+  /**
+   * @param $direction
+   *
+   * @return array|mixed[]|null
+   * @throws \Drupal\Core\Entity\EntityMalformedException
+   */
+  private function buildQuery($direction = 'prev') {
+    $currentNode = $this->routeMatch->getParameter('node');
+    $query = $this->nodeStorage->getQuery();
+
+    $compare = $direction == 'prev' ? '<' : '>';
+    $sort = $direction == 'prev' ? 'DESC' : 'ASC';
+
+    $result = $query->condition('created', $currentNode->getCreatedTime(), $compare)
+                 ->condition('type', $currentNode->bundle())
+                 ->condition('status', 1)
+                 ->condition('langcode', $currentNode->language()->getId())
+                 ->sort('created', $sort)
+                 ->range(0, 1)
+                 ->execute();
+
+    if (!empty($result) && is_array($result)) {
+      $nid = array_shift($result);
+      $node = $this->nodeStorage->load($nid);
+
+      $link = Link::fromTextAndUrl($node->label(), $node->toUrl());
+      $link = $link->toRenderable();
+      return $link;
+    }
+
+    return null;
+  }
+
+  /**
+   * @return array|mixed[]|null
+   * @throws \Drupal\Core\Entity\EntityMalformedException
+   */
+  public function getPrevLink() {
+    return $this->buildQuery('prev');
+  }
+
+  /**
+   * @return array|mixed[]|null
+   * @throws \Drupal\Core\Entity\EntityMalformedException
+   */
+  public function getNextLink() {
+    return $this->buildQuery('next');
+  }
+
+  /**
+   * @return array
+   * @throws \Drupal\Core\Entity\EntityMalformedException
+   */
+  public function build() {
+    $prevLink = $this->getPrevLink();
+    $nextLink = $this->getNextLink();
+    $class = ($prevLink && $nextLink) ? 'both' : '';
+    return [
+      '#theme' => 'simple_pagination',
+      '#prev_link' => $prevLink,
+      '#next_link' => $nextLink,
+      '#wrapperClass' => $class,
+    ];
+  }
+
+}
