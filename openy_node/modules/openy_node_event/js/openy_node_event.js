3e8598492b8e954ca6649152ed02d05230edd1cd
Avi Schwab <froboy@gmail.com>
2023-07-07 14:12:38 -0500

    DS-858 fix: DS-858 Move addtocalendar js to openy_system

--- openy_node/modules/openy_node_event/js/openy_node_event.js
+++ openy_node/modules/openy_node_event/js/openy_node_event.js
@@ -22,25 +22,4 @@
     }
   };
 
-  Drupal.behaviors.atcFix = {
-    attach: function (context, settings) {
-      $('.addtocalendar').click(function() {
-        if ($(this).hasClass('activated')) {
-          $(this).removeClass('activated');
-          $(this).find('.atcb-list').css({
-            'display' : 'none',
-            'visibility' : 'hidden'
-          });
-        }
-        else {
-          $(this).addClass('activated');
-          $(this).find('.atcb-list').css({
-            'display' : 'block',
-            'visibility' : 'visible'
-          });
-        }
-      });
-    }
-  };
-
 })(jQuery);
